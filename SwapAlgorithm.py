6ff75414d44ef8fa85710b9ab282aad11fe55157
gauss49 <99512748+gauss49@users.noreply.github.com>
2024-03-27 00:29:38 -0500

    Add files via upload

--- SwapAlgorithm.py
+++ SwapAlgorithm.py
@@ -4,11 +4,16 @@ from matplotlib import pyplot as plt
 import Keating
 import time
 
+def algorithm(vertex_models):
+    for vertex in vertex_models:
+        SwapPaths = Vertex_Algorithm(vertex)
+
 # Naive Implementation, how I would do it by hand
-def Algorithm(vertex):
+def Vertex_Algorithm(vertex):
     SwapPaths = []
     for path in vertex.paths:
         alg_path = path_algorithm(vertex, path)
+        # VertexModel.Graph(VertexModel.Vertex([path]))
         if alg_path is not None:
             SwapPaths.append(alg_path)
     return SwapPaths
@@ -38,7 +43,14 @@ def path_algorithm(vertex, start_path):
                     enter, exit = (currEnterExit[1], paths.EnterExit[square][1]) if currPath.color == "red" else (currEnterExit[0], paths.EnterExit[square][0])
                     if enter != exit:
                         currPath = paths
+                        break
         currEnterExit = currPath.EnterExit[currSquare]
+        if currPath.color == "blue":
+            if currSquare[1] == vertex.size[1]-1 and currEnterExit[1] == 1:
+                break
+        else:
+            if currSquare[1] == 0 and currEnterExit[1] == 1:
+                break
         if currPath.color == "red":
             currSquare = currPath.next_square(currSquare, reverse = True)
             if currSquare == -1:
@@ -50,14 +62,18 @@ def path_algorithm(vertex, start_path):
                 alg_path = VertexModel.Path(squares = SwapPath, color = "black")
                 return alg_path
         SwapPath.append(currSquare)
-        if currPath.color == "blue":
-            if currSquare[1] == vertex.size[1]-1 and currEnterExit[1] == 1:
-                break
-        else:
-            if currSquare[1] == 0 and currEnterExit[0] == 1:
-                break
     alg_path = VertexModel.Path(squares = SwapPath, color = "black")
     return alg_path
+
+# Returns a tuple of 4 elements that describes the start and end square.
+def get_start_end(vertex):
+    boundaries = []
+    for path in vertex.paths:
+        start_square = path.squares[0]
+        end_square = path.squares[-1]
+        # In case order doesn't matter, you can just turn these into sets
+        boundaries.append((start_square[0], start_square[1], end_square[0], end_square[1]))
+    return boundaries
 
 def Graph(vertex, indiv = False):
     if indiv:
@@ -130,40 +146,3 @@ def direction(point1, point2):
         return "red"   # Moving left or down
     else:
         return "black"  # No movement
-
-# Example case from the paper
-b1 = VertexModel.Path([(0,0),(0,1), (0,2), (1,2), (2,2), (2,3), (2,4), (3,4), (4,4), (4,5), (5,5), (6,5), (6,6)], "blue")
-r1 = VertexModel.Path([(0,0), (0,1), (0,2), (0,3), (0,4), (1,4), (2,4), (2,5), (2,6)],"red")
-b2 = VertexModel.Path([(1,0), (1,1), (2,1), (3,1), (4,1), (5,1), (5,2), (5,3), (5,4), (6,4), (7,4), (8,4), (8,5), (8,6)], "blue")
-r2 = VertexModel.Path([(1,0), (1,1), (1,2), (1,3), (2,3), (3,3), (3,4), (3,5), (4,5), (4,6)], "red")
-b3 = VertexModel.Path([(2,0), (3,0), (4,0), (5,0), (6,0), (7,0), (8,0), (9,0), (10,0), (10,1), (10,2), (10,3), (10,4), (10,5), (10,6)],"blue")
-r3 = VertexModel.Path([(4,0), (4,1), (4,2), (4,3), (4,4), (5,4), (5,5), (5,6), (6,6)], "red")
-v1 = VertexModel.Vertex([b1, r1, b2, r2, b3, r3])
-
-# Uncommenting this gives you the first path detailed in the paper
-# v2 = VertexModel.Vertex([path_algorithm(v1, v1.RedPaths[0])])
-# VertexModel.Graph(v2)
-# v2 = VertexModel.Vertex([path_algorithm(v1, v1.RedPaths[1])])
-# VertexModel.Graph(v2)
-# v2 = VertexModel.Vertex([path_algorithm(v1, v1.RedPaths[2])])
-# VertexModel.Graph(v2)
-# v2 = VertexModel.Vertex([path_algorithm(v1, v1.BluePaths[0])])
-# VertexModel.Graph(v2)
-# v2 = VertexModel.Vertex([path_algorithm(v1, v1.BluePaths[1])])
-# VertexModel.Graph(v2)
-# v2 = VertexModel.Vertex([path_algorithm(v1, v1.BluePaths[2])])
-# print(path_algorithm(v1, v1.BluePaths[2]).pathing)
-# VertexModel.Graph(v2)
-# This gives all path ouputs according to the algorithm for the vertex model described
-# s = time.perf_counter()
-# v2 = VertexModel.Vertex(Algorithm(v1))
-# print(time.perf_counter()-s)
-# Graph(v2, indiv = False)
-
-# for partition in Keating.Partition([[2,2],[2]],[[2,1],[0]]).TabList(2):
-#     print(partition[0], partition[1])
-#     print(VertexModel.KeatingToJiang(partition, 2))
-#     VertexModel.Graph(VertexModel.KeatingToJiang(partition, 2))
-# l = []
-# l = Algorithm(l)
-# Assume this gives us all vertex model objects after running the swap algorithm on them all.
